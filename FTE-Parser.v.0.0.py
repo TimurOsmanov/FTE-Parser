f9dfb9ae34b8ad0455c1f217e4abb104530cd815
TimurOsmanov <102800823+TimurOsmanov@users.noreply.github.com>
2022-07-29 20:09:45 +0300

    Added function get_workers_names, in function check_answers changed var report to use in it full name of worker, added function add_new_member_to_workers_info, added creating of new table 'workers_info', changed message_handlers (state=Form.final_answer and state=Form.final_answer2), added function checking, added handler for new users

--- FTE-Parser.v.0.0.py
+++ FTE-Parser.v.0.0.py
@@ -19,8 +19,8 @@ import sqlite3
 # Now you can use app
 # 3. Bot will work correctly if you create group and then add bot there
 bot_token, api_hash = '', ''
-api_id, group_id, user_id, manager_group_id = 0, 0, 0, 0
-polls, chosen_projects, question_index, report = {}, {}, {}, {}
+api_id, group_id, user_id, manager_group_id, new_user_id = 0, 0, 0, 0, 0
+polls, chosen_projects, question_index, report, workers_in_db = {}, {}, {}, {}, {}
 projects_set_by_admin, chat_members, chat_admins = [], [], [0, 0]
 bot = Bot(bot_token)
 storage = MemoryStorage()
@@ -47,10 +47,20 @@ def get_worker_daily_stat():
         return number[-1][0]
 
 
+def get_workers_names():
+    global workers_in_db
+    with sqlite3.connect('sqlite_python.db') as conn2:
+        cursor2 = conn2.cursor()
+        select_info = 'SELECT user_id, full_name FROM workers_info'
+        cursor2.execute(select_info)
+        names = cursor2.fetchall()
+        workers_in_db = {user[0]: user[1] for user in names}
+
+
 def insert_into_db(data_tuple):
     # Function inserts new record according to new answer in poll
-    with sqlite3.connect('sqlite_python.db') as conn2:
-        cursor3 = conn2.cursor()
+    with sqlite3.connect('sqlite_python.db') as conn3:
+        cursor3 = conn3.cursor()
         insert_info = '''INSERT INTO FTE_info (unique_id, date, project, 
         user_id, hours) VALUES (?, ?, ?, ?, ?)'''
         cursor3.execute(insert_info, data_tuple)
@@ -58,34 +68,40 @@ def insert_into_db(data_tuple):
 
 def check_answers():
     global report
-    with sqlite3.connect('sqlite_python.db') as conn3:
+    with sqlite3.connect('sqlite_python.db') as conn4:
         now = (f'{datetime.datetime.now().year}-{datetime.datetime.now().month}-'
                f'{datetime.datetime.now().day}')
-        cursor1 = conn3.cursor()
+        cursor4 = conn4.cursor()
         select_info = f"SELECT user_id, SUM(hours) FROM FTE_info WHERE date = '{now}' GROUP BY user_id"
-        cursor1.execute(select_info)
-        daily_stat_raw = cursor1.fetchall()
+        cursor4.execute(select_info)
+        daily_stat_raw = cursor4.fetchall()
         daily_stat = {user[0]: user[1] for user in daily_stat_raw}
         for worker in chat_members:
             if worker in daily_stat:
                 if not 6 <= daily_stat[worker] <= 12:
-                    report[worker] = daily_stat[worker]
+                    report[workers_in_db[worker]] = daily_stat[worker]
             else:
-                report[worker] = 'не ответил/ не работал'
-        # change worker to full name of worker
-        # must work async
+                report[workers_in_db[worker]] = 'не ответил/не работал'
+        return report
+
+
+def add_new_member_to_workers_info(data_tuple):
+    with sqlite3.connect('sqlite_python.db') as conn5:
+        cursor5 = conn5.cursor()
+        insert_info = '''INSERT INTO workers_info (user_id, full_name) VALUES (?, ?)'''
+        cursor5.execute(insert_info, data_tuple)
 
 
 try:
-    with sqlite3.connect('sqlite_python.db') as conn4:
-        cursor2 = conn4.cursor()
+    with sqlite3.connect('sqlite_python.db') as conn6:
+        cursor6 = conn6.cursor()
         create_table = '''CREATE TABLE FTE_info (
                                 unique_id INTEGER PRIMARY KEY,
                                 date datetime,
                                 project text,
                                 user_id INTEGER,
                                 hours REAL NOT NULL)'''
-        cursor2.execute(create_table)
+        cursor6.execute(create_table)
         worker_daily_stat = 0
 except sqlite3.Error as error:
     # error.args ('table FTE_info already exists',) is a system message when your DB already has table
@@ -98,13 +114,25 @@ except sqlite3.Error as error:
     else:
         print("Ошибка при подключении к sqlite", error)
 
+try:
+    with sqlite3.connect('sqlite_python.db') as conn7:
+        cursor7 = conn7.cursor()
+        create_table2 = '''CREATE TABLE workers_info (user_id INTEGER PRIMARY KEY, full_name text)'''
+        cursor7.execute(create_table2)
+except sqlite3.Error as error:
+    # error.args ('table workers_info already exists',) is a system message when your DB already has table
+    if error.args == ('table workers_info already exists',):
+        print("Напоминаю, таблица работников уже создана", error)
+
 
 class Form(StatesGroup):
     project = State()
     project_name = State()
     answer = State()
     final_answer = State()
+    final_answer2 = State()
     end = State()
+    first_message = State()
 
 
 @dp.message_handler(commands=['set_projects'])
@@ -131,11 +159,11 @@ async def admin_set_projects(message: types.Message, state: FSMContext):
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     # Attribute of function cant be removed cause without it function raises error
-    await create_1st_poll()
-    # aioschedule.every().day.at('08:00').do(create_1st_poll)
-    # while True:
-    #     await aioschedule.run_pending()
-    #     await asyncio.sleep(1)
+    aioschedule.every().day.at('08:00').do(create_1st_poll)
+    aioschedule.every().day.at('09:00').do(checking)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
 
 
 async def create_1st_poll():
@@ -163,42 +191,69 @@ async def voting(call):
 async def create_questions_from_polls(message: types.Message, state: FSMContext):
     # Attribute of function cant be removed cause without it function raises error
     global user_id, question_index, chosen_projects
+    markup_remove = types.ReplyKeyboardRemove()
     question_index[user_id] = 0
-    await Form.answer.set()
     try:
         project = chosen_projects[user_id][question_index[user_id]]
-        await bot.send_message(user_id, f'Сколько часов вы были заняты на проекте {project} вчера?')
+        await bot.send_message(user_id, f'Сколько часов вы были заняты на проекте {project} вчера?',
+                               reply_markup=markup_remove)
         polls[user_id] = project
         question_index[user_id] += 1
-        await Form.final_answer.set()
+        await Form.final_answer2.set()
     except IndexError:
         await bot.send_message(user_id, f'Произошла ошибка, обратитесь к администратору')
         await state.finish()
 
 
 @dp.message_handler(lambda message: message.text not in ["Продолжить"], state=Form.final_answer)
+async def create_questions_from_polls(message: types.Message):
+    # Attribute of function cant be removed cause without it function raises error
+    await bot.send_message(user_id, f"Вам нужно нажать кнопку 'Продолжить' или написать"
+                                    f" Продолжить ")
+    await Form.final_answer.set()
+
+
+@dp.message_handler(state=Form.final_answer2)
 async def create_questions_from_polls(message: types.Message, state: FSMContext):
     global user_id, question_index, worker_daily_stat, user_id, chosen_projects
-    async with state.proxy() as data:
-        data['answer'] = message.text
-    hours = data['answer']
-    worker_daily_stat += 1
-    now = (f'{datetime.datetime.now().year}-{datetime.datetime.now().month}-'
-           f'{datetime.datetime.now().day}')
-    data_tuple = (worker_daily_stat, now, polls[user_id], user_id, hours)
-    insert_into_db(data_tuple)
-    await Form.answer.set()
-    try:
-        project = chosen_projects[user_id][question_index[user_id]]
-        await bot.send_message(user_id, f'Сколько часов вы были заняты на проекте {project} вчера?')
-        polls[user_id] = project
-        question_index[user_id] += 1
-        await Form.final_answer.set()
-    except IndexError:
-        question_index = {}
-        await bot.send_message(user_id, f'Спасибо, ваши данные учтены')
-        user_id = 0
-        await state.finish()
+    replaced_message_text = message.text.replace(',', '').replace('.', '')
+    if not replaced_message_text.isdigit():
+        await bot.send_message(user_id, f'Ответ должен быть числом')
+        await Form.final_answer2.set()
+    else:
+        async with state.proxy() as data:
+            data['answer'] = message.text
+        hours = data['answer']
+        worker_daily_stat += 1
+        now = (f'{datetime.datetime.now().year}-{datetime.datetime.now().month}-'
+               f'{datetime.datetime.now().day}')
+        data_tuple = (worker_daily_stat, now, polls[user_id], user_id, hours.replace(',', '.'))
+        insert_into_db(data_tuple)
+        try:
+            project = chosen_projects[user_id][question_index[user_id]]
+            await bot.send_message(user_id, f'Сколько часов вы были заняты на проекте {project} вчера?')
+            polls[user_id] = project
+            question_index[user_id] += 1
+            await Form.final_answer2.set()
+        except IndexError:
+            question_index = {}
+            await bot.send_message(user_id, f'Спасибо, ваши данные учтены')
+            user_id = 0
+            await state.finish()
+
+
+async def checking():
+    global report
+    if report == {}:
+        get_workers_names()
+        check_answers()
+    for key, value in report.items():
+        if isinstance(value, float):
+            report[key] = round(float(value), 2)
+        else:
+            report[key] = value
+    await bot.send_message(manager_group_id, f'{report}')
+    report = {}
 
 
 async def greetings():
@@ -209,9 +264,31 @@ async def greetings():
 
 @dp.message_handler(content_types=['new_chat_members'])
 async def new_user_joined(message: types.Message):
-    for new_member in message.new_chat_members:
-        chat_members.append(new_member.id)
-        await greetings()
+    global group_id, new_user_id
+    if message.chat.id == group_id:
+        for new_member in message.new_chat_members:
+            chat_members.append(new_member.id)
+            new_user_id = new_member.id
+            await greetings()
+
+
+@dp.message_handler()
+async def first_message_to_bot(message: types.Message):
+    global new_user_id
+    await Form.first_message.set()
+    await bot.send_message(new_user_id, 'Напишите свое ФИО')
+
+
+@dp.message_handler(state=Form.first_message)
+async def first_message_to_bot(message: types.Message, state: FSMContext):
+    global new_user_id, workers_in_db
+    if new_user_id not in workers_in_db:
+        data_tuple = (new_user_id, message.text)
+        add_new_member_to_workers_info(data_tuple)
+        await bot.send_message(new_user_id, 'Вы добавлены в базу')
+        await state.finish()
+    else:
+        await state.finish()
 
 
 if __name__ == '__main__':
